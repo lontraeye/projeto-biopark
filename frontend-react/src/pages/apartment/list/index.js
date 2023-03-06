426d12ce9906fded4302f31a5e9093276dc9bdc5
lontraeye <lucascardozo73@gmail.com>
2023-03-06 07:39:30 -0300

    removing unused files

--- frontend-react/src/pages/apartment/list/index.js
+++ frontend-react/src/pages/apartment/list/index.js
@@ -7,13 +7,12 @@ import {
     Typography,
 } from "@mui/material";
 import { useEffect, useState } from "react";
-import { APARTMENT, BUILDING } from "../../../constants/url";
+import { APARTMENT } from "../../../constants/url";
 import { get } from "../../../utils/restUtils";
 
 import "../../apartment/list/style.css";
 
 export default function ApartmentsList() {
-    const [buildings, setBuildings] = useState([]);
     const [apartments, setApartments] = useState([]);
 
     useEffect(() => {
@@ -24,56 +23,59 @@ export default function ApartmentsList() {
     }, []);
 
     const initLists = async () => {
-        let buildingsTemp = await get(BUILDING);
-        setBuildings(buildingsTemp);
         let apartmentsTemp = await get(APARTMENT);
         setApartments(apartmentsTemp);
     };
-    
+
     return (
         <div className="cards">
             {apartments.map((apartment) => {
-                const isRented = Boolean(apartment.tenant)
+                const isRented = Boolean(apartment.tenant);
                 return (
-                <div key={apartment.name}>
-                    <div>
-                        <Card className="apartmentCard">
-                            <CardContent>
-                                <div className="firstSet">
-                                    <Typography
-                                        gutterBottom
-                                        variant="h5"
-                                        component="div"
-                                    >
-                                        {apartment.name}
-                                    </Typography>
+                    <div key={apartment.name}>
+                        <div>
+                            <Card className="apartmentCard">
+                                <CardContent>
+                                    <div className="firstSet">
+                                        <Typography
+                                            gutterBottom
+                                            variant="h5"
+                                            component="div"
+                                        >
+                                            {apartment.name}
+                                        </Typography>
+                                        <Typography
+                                            variant="body2"
+                                            color="text.secondary"
+                                        >
+                                            {apartment.description}
+                                        </Typography>
+                                    </div>
                                     <Typography
                                         variant="body2"
                                         color="text.secondary"
                                     >
-                                        {apartment.description}
+                                        Aluguel: ${apartment.rentValue}
                                     </Typography>
-                                </div>
-                                <Typography
-                                    variant="body2"
-                                    color="text.secondary"
-                                >
-                                    Aluguel: ${apartment.rentValue}
-                                </Typography>
-                                <Divider />
-                                <div className="secondSet">
-                                    <Typography>                                        Situação:
-                                        {isRented ? "alugado" : "disponivel"}
-                                    </Typography>
-                                </div>
-                            </CardContent>
-                            <CardActions className="buttons">
-                                <Button size="small">Alugar</Button>
-                            </CardActions>
-                        </Card>
+                                    <Divider />
+                                    <div className="secondSet">
+                                        <Typography>
+                                            {" "}
+                                            Situação:
+                                            {isRented
+                                                ? "Alugado"
+                                                : "Disponivel"}
+                                        </Typography>
+                                    </div>
+                                </CardContent>
+                                <CardActions className="buttons">
+                                    <Button size="small">Alugar</Button>
+                                </CardActions>
+                            </Card>
+                        </div>
                     </div>
-                </div>
-            )})}
+                );
+            })}
         </div>
     );
 }
